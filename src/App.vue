9a91d1bb49e4c3ddae78e9f84f1a6d1f211bc5f4
steven <radishj@gmail.com>
2020-04-04 22:39:48 -0700

    remove public folder

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="black"
+      dark
+      height="82px"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./imgs/tasti-icon-light.png"
+          transition="scale-transition"
+          width="80"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="text-center">
+        <v-btn rounded class="hover_color font-weight-black hover_text" large outlined dark width="180px">Apply Now</v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+.hover_color:hover{
+  background: #66b873;
 }
-
-#nav {
-  padding: 30px;
+.hover_text{
+  color:rgb(112, 207, 112);
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.hover_text:hover{
+  color: white;
 }
 </style>
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
